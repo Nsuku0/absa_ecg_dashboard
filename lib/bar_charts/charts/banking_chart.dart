169a63fa3dba6f959bf4b6046baa9b38262c2dd1
Nsuku Minyuku <minyukunsuku0@gmail.com>
2022-06-27 04:07:29 +0200

    Shortened chart titles

--- lib/bar_charts/charts/banking_chart.dart
+++ lib/bar_charts/charts/banking_chart.dart
@@ -6,68 +6,78 @@ class BankingChart extends StatelessWidget {
   List<charts.Series> seriesList = _createSampleData();
   final bool animate = true;
 
-  BankingChart({Key? key}) : super(key: key);
+  BankingChart();
 
   factory BankingChart.withSampleData() {
-    return BankingChart();
+    return new BankingChart();
   }
 
   @override
   Widget build(BuildContext context) {
-    return charts.BarChart(
+    return new charts.BarChart(
       seriesList = _createSampleData(),
       animate: animate,
-      //barGroupingType: charts.BarGroupingType.stacked,
+      barGroupingType: charts.BarGroupingType.stacked,
+      defaultRenderer: charts.BarRendererConfig(
+          cornerStrategy: const charts.ConstCornerStrategy(2)),
+
       // Add the series legend behavior to the chart to turn on series legends.
       // By default the legend will display above the chart.
-      //behaviors: [charts.SeriesLegend()],
+      behaviors: [
+        charts.ChartTitle(
+            'Scope Emissions for the Banking sector in 2020(left bar) and 2021(right bar)',
+            behaviorPosition: charts.BehaviorPosition.bottom,
+            titleStyleSpec:   const charts.TextStyleSpec(fontSize: 14,),
+            titleOutsideJustification:
+            charts.OutsideJustification.middleDrawArea),
+      ],
     );
   }
 
   /// Create series list with multiple series
   static List<charts.Series<Emissions, String>> _createSampleData() {
     final fnb_2020 = [
-      Emissions('FNB', 8301), //1 2020
-      Emissions('FNB', 171), // 2 2020
+      new Emissions('FNB', 8301), //1 2020
+      new Emissions('FNB', 171), // 2 2020
     ];
 
     final fnb_2021 = [
-      Emissions('FNB', 6507), // 1 2021
-      Emissions('FNB', 153), //2 2021
+      new Emissions('FNB', 6507), // 1 2021
+      new Emissions('FNB', 153), //2 2021
     ];
 
     final nedbank_2020 = [
-      Emissions('Nedbank ', 1265), // 1 2020
-      Emissions('Nedbank ', 118050), //2 2020
+      new Emissions('Nedbank ', 1265), // 1 2020
+      new Emissions('Nedbank ', 118050), //2 2020
     ];
 
     final nedbank_2021 = [
-      Emissions('Nedbank ', 1541), // 1 2021
-      Emissions('Nedbank', 110529), //2 2021
+      new Emissions('Nedbank ', 1541), // 1 2021
+      new Emissions('Nedbank', 110529), //2 2021
     ];
     return [
-      charts.Series<Emissions, String>(
+      new charts.Series<Emissions, String>(
         id: 'FNB (2020)',
         colorFn: (_, __) => charts.ColorUtil.fromDartColor(pinks[3]),
         domainFn: (Emissions emission, _) => emission.name,
         measureFn: (Emissions emission, _) => emission.emissions,
         data: fnb_2020,
       ),
-      charts.Series<Emissions, String>(
+      new charts.Series<Emissions, String>(
         id: 'FNB (2021)',
         colorFn: (_, __) => charts.ColorUtil.fromDartColor(pinks[2]),
         domainFn: (Emissions emission, _) => emission.name,
         measureFn: (Emissions emission, _) => emission.emissions,
         data: fnb_2021,
       ),
-      charts.Series<Emissions, String>(
+      new charts.Series<Emissions, String>(
         id: 'Nedbank (2020)',
         colorFn: (_, __) => charts.ColorUtil.fromDartColor(pinks[1]),
         domainFn: (Emissions emission, _) => emission.name,
         measureFn: (Emissions emission, _) => emission.emissions,
         data: nedbank_2020,
       ),
-      charts.Series<Emissions, String>(
+      new charts.Series<Emissions, String>(
         id: 'Nedbank (2021)',
         colorFn: (_, __) => charts.ColorUtil.fromDartColor(pinks[0]),
         domainFn: (Emissions emission, _) => emission.name,
