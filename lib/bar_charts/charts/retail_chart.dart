977ad57a01720dde99a2c45ff499e1dcbd7251c2
Nsuku Minyuku <minyukunsuku0@gmail.com>
2022-06-27 04:12:45 +0200

    shortened chart title

--- lib/bar_charts/charts/retail_chart.dart
+++ lib/bar_charts/charts/retail_chart.dart
@@ -6,94 +6,104 @@ class RetailChart extends StatelessWidget {
   List<charts.Series> seriesList = _createSampleData();
   final bool animate = true;
 
-  RetailChart({Key? key}) : super(key: key);
+  RetailChart();
 
   factory RetailChart.withSampleData() {
-    return RetailChart();
+    return new RetailChart();
   }
 
   @override
   Widget build(BuildContext context) {
-    return charts.BarChart(
+    return new charts.BarChart(
       seriesList = _createSampleData(),
       animate: animate,
-      //barGroupingType: charts.BarGroupingType.stacked,
+      barGroupingType: charts.BarGroupingType.stacked,
       // Add the series legend behavior to the chart to turn on series legends.
       // By default the legend will display above the chart.
-      //behaviors: [charts.SeriesLegend()],
+      defaultRenderer: charts.BarRendererConfig(
+          cornerStrategy: const charts.ConstCornerStrategy(2)),
+
+      behaviors: [
+        charts.ChartTitle(
+            'Scope Emissions for the Retail sector in 2020(left bar) and 2021(right bar)',
+            behaviorPosition: charts.BehaviorPosition.bottom,
+            titleStyleSpec:   const charts.TextStyleSpec(fontSize: 14,),
+            titleOutsideJustification:
+            charts.OutsideJustification.middleDrawArea),
+      ],
     );
   }
 
   /// Create series list with multiple series
   static List<charts.Series<Emissions, String>> _createSampleData() {
     final woolies_2020 = [
-      Emissions('Woolworths', 44886), //1 2020
-      Emissions('Woolworths', 441875), //2 2020
+      new Emissions('Woolworths Holdings Limited', 44886), //1 2020
+      new Emissions('Woolworths Holdings Limited', 441875), //2 2020
     ];
 
     final woolies_2021 = [
-      Emissions('Woolworths', 449819), //1 2021
-      Emissions('Woolworths', 40675226), //2 2021
+      new Emissions('Woolworths Holdings Limited', 449819), //1 2021
+      new Emissions('Woolworths Holdings Limited', 40675226), //2 2021
     ];
 
     final mr_price_2020 = [
-      Emissions('Mr Price', 52535), //1 2020
-      Emissions('Mr Price', 52535), //2 2020
+      new Emissions('Mr Price Group LTD', 52535), //1 2020
+      new Emissions('Mr Price Group LTD', 52535), //2 2020
     ];
 
     final mr_price_2021 = [
-      Emissions('Mr Price', 46034), //1 2021
+      new Emissions('Mr Price Group LTD', 46034), //1 2021
 
-      Emissions('Mr Price', 46034), //2 2021
+      new Emissions('Mr Price Group LTD', 46034), //2 2021
     ];
 
     final shoprite_2020 = [
-      Emissions('Shoprite', 528889), //1 2020
-      Emissions('Shoprite', 1802314), //2 2020
+      new Emissions('Shoprite Holdings', 528889), //1 2020
+      new Emissions('Shoprite Holdings', 1802314), //2 2020
     ];
 
     final shoprite_2021 = [
-      Emissions('Shoprite', 511067), //1 2021
-      Emissions('Shoprite', 1694193), //2 2021
+      new Emissions('Shoprite Holdings', 511067), //1 2021
+      new Emissions('Shoprite Holdings', 1694193), //2 2021
     ];
 
     return [
-      charts.Series<Emissions, String>(
+      new charts.Series<Emissions, String>(
         id: 'Woolworths (2020)',
         colorFn: (_, __) => charts.ColorUtil.fromDartColor(pinks[0]),
         domainFn: (Emissions emission, _) => emission.name,
         measureFn: (Emissions emission, _) => emission.emissions,
         data: woolies_2020,
       ),
-      charts.Series<Emissions, String>(
+      new charts.Series<Emissions, String>(
         id: 'Woolworths (2020)',
         colorFn: (_, __) => charts.ColorUtil.fromDartColor(pinks[1]),
         domainFn: (Emissions emission, _) => emission.name,
         measureFn: (Emissions emission, _) => emission.emissions,
         data: woolies_2021,
       ),
-      charts.Series<Emissions, String>(
-        id: 'MrPrice (2020)',
+      new charts.Series<Emissions, String>(
+        id: 'Mr Price (2020)',
         colorFn: (_, __) => charts.ColorUtil.fromDartColor(pinks[2]),
         domainFn: (Emissions emission, _) => emission.name,
         measureFn: (Emissions emission, _) => emission.emissions,
         data: mr_price_2020,
       ),
-      charts.Series<Emissions, String>(
-        id: 'MrPrice (2021)',
+      new charts.Series<Emissions, String>(
+        id: 'Mr Price (2021)',
         colorFn: (_, __) => charts.ColorUtil.fromDartColor(pinks[3]),
         domainFn: (Emissions emission, _) => emission.name,
         measureFn: (Emissions emission, _) => emission.emissions,
         data: mr_price_2021,
       ),
-      charts.Series<Emissions, String>(
+      new charts.Series<Emissions, String>(
         id: 'Shoprite (2020)',
         colorFn: (_, __) => charts.ColorUtil.fromDartColor(pinks[2]),
         domainFn: (Emissions emission, _) => emission.name,
         measureFn: (Emissions emission, _) => emission.emissions,
         data: shoprite_2020,
       ),
-      charts.Series<Emissions, String>(
+      new charts.Series<Emissions, String>(
         id: 'Shoprite (2021)',
         colorFn: (_, __) => charts.ColorUtil.fromDartColor(pinks[1]),
         domainFn: (Emissions emission, _) => emission.name,
